5518fbf462fe24bd1f76e87bf3611ac0d1a40374
EpLLI <staniush.iv@gmail.com>
2016-01-12 20:39:07 +0300

    %- стал + +стал-%

--- test/src/main/java/by/pvt/test/Calc.java
+++ test/src/main/java/by/pvt/test/Calc.java
@@ -5,13 +5,13 @@ public class Calc {
 	public static int Pluse(int a,int b){
 				
 		
-		return a+b;
+		return a-b;
 		
 	}
 public static int Minus(int a,int b){
 				
 		
-		return a-b;
+		return a+b;
 		
 	}
 	
